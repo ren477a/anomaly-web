4925b1ae74650062e4efd7c791df30b1b326d583
Dave Martinez <dave.martinez25@gmail.com>
2019-03-21 23:59:48 +0800

    manually adjust timezone +8 hours

--- src/MainContent/EventItem.js
+++ src/MainContent/EventItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 
 class EventItem extends React.Component {
   render() {
@@ -8,7 +9,7 @@ class EventItem extends React.Component {
         <td>{ this.props.name } occured at <br /><strong className="has-text-info"> { this.props.location } </strong></td>
         <td>
           {/* <p className="has-text-right">January 31, 2019<br />13:24:56</p> */}
-          <p className="has-text-right"> { this.props.time }</p>
+          <p className="has-text-right"> { moment(this.props.time).add(8, 'hours').fromNow() }</p>
         </td>
       </tr>
     )
