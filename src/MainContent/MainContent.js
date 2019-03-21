3bdb716d093b0a9929228156ffe3089630c6a1dc
Dave Martinez <dave.martinez25@gmail.com>
2019-03-21 23:52:30 +0800

    add dynamic date

--- src/MainContent/MainContent.js
+++ src/MainContent/MainContent.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import Tiles from './Tiles';
+import moment from 'moment'
 import RecentEvents from './RecentEvents';
 import IncidentSearch from './IncidentSearch';
 
-function MainContent() {
-  return (
-    <div className="column is-9">
-        <section className="hero is-info welcome is-small">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                Summary Report
-              </h1>
-              <h2 className="subtitle">
-                January 31, 2019
-              </h2>
+class  MainContent extends React.Component {
+  constructor(props) {
+    super(props)
+
+    var currentDate = moment().format('MMMM D, YYYY')
+
+    this.state = {
+      currentDate: currentDate
+    }
+  }
+  
+  render() {
+    return (
+      <div className="column is-9">
+          <section className="hero is-info welcome is-small">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Summary Report
+                </h1>
+                <h2 className="subtitle">
+                  { this.state.currentDate }
+                </h2>
+              </div>
             </div>
+          </section>
+  
+          <Tiles />
+  
+          <div className="columns">
+              <RecentEvents />
+              <IncidentSearch />
           </div>
-        </section>
-
-        <Tiles />
-
-        <div className="columns">
-            <RecentEvents />
-            <IncidentSearch />
         </div>
-      </div>
-  )
+    )
+  }
 }
 
 export default MainContent;
