5da6fae327b17ff4080aa612ccda2002bc0d0af3
Efren Mercado Jr <efren.mercado.jr@dynameyes.com>
2019-05-06 10:24:23 +0800

    Alerts when success/failed register. Redirect to login

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom'
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -9,10 +10,23 @@ class SignupForm extends React.Component {
       username: '',
       email: '',
       password: '',
+      redirect: false,
     }
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/login' />
+    }
   }
 
   onChange(e) {
@@ -21,11 +35,24 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    
-    axios.post('http://127.0.0.1:8000/useraccounts/users/', this.state)
+    let register_data = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(register_data)
+    axios.post('http://127.0.0.1:8000/useraccounts/users/', register_data)
       .then(res => {
-        console.log(res);
+        console.log(res.status);
+        if (res.status === 201) {
+          alert("Registration successful!")
+          this.setRedirect();
+        }
       })
+      .catch( err => {
+        console.log(err)
+        alert("Username/Email is already taken!");
+      });
   }
 
   render() {
@@ -66,6 +93,7 @@ class SignupForm extends React.Component {
                   value={this.state.password}/>
             </div>
         </div>
+        {this.renderRedirect()}
         <button type="submit" className="button is-block is-success is-large is-fullwidth">Register</button>
       </form>
     )
